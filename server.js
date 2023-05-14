32b04c8bb0e615e5dabb8616ca9dcce3c79fcea2
BogdanGlazkov <bglazkov@i.ua>
2023-05-14 19:39:31 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const studentRoutes = require("./src/student/routes");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/students", studentRoutes);
+
+app.listen(port, () => console.log(`App is running on port ${port}`));
