bad211a1b3cdead9a5cf16f494fd1cbf040995b4
BogdanGlazkov <bglazkov@i.ua>
2023-05-14 19:57:48 +0300

    Add GET method

--- src/student/routes.js
+++ src/student/routes.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
+const db = require("./controller");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Using API route");
-});
+router.get("/", db.getStudents);
 
 module.exports = router;
