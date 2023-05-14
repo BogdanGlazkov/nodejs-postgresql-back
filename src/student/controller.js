2520b69675cb4088e96d8a74f927d19ea9dcd2b7
BogdanGlazkov <bglazkov@i.ua>
2023-05-14 21:06:11 +0300

    Add GET by id method

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,10 +1,20 @@
 const pool = require("../../db");
+const queries = require("./queries");
 
 const getStudents = (req, res) => {
-  pool.query("SELECT * FROM students", (error, result) => {
+  pool.query(queries.getStudents, (error, result) => {
     if (error) throw error;
     res.status(200).json(result.rows);
   });
 };
 
-module.exports = { getStudents };
+const getStudentById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query(queries.getStudentById, [id], (error, result) => {
+    if (error) throw error;
+    res.status(200).json(result.rows);
+  });
+};
+
+module.exports = { getStudents, getStudentById };
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,5 +4,6 @@ const db = require("./controller");
 const router = Router();
 
 router.get("/", db.getStudents);
+router.get("/:id", db.getStudentById);
 
 module.exports = router;
